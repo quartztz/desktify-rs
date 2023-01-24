54021179424451853dd364aea29bba5b5764042a
quartztz <jacopo.moretti@epfl.ch>
2023-01-24 14:09:15 +0100

    ugly but works

--- src/desktify-get.py
+++ src/desktify-get.py
@@ -9,19 +9,33 @@ import spotipy.util as util
 import os
 
 PATH_TO_IMAGE = "./assets/album_img.png"
+PATH_TO_INFO  = "./assets/info.txt"
+
+def write_info(info): 
+  with open(PATH_TO_INFO, "w+") as f: 
+    f.write(info)
+
+def write_img(image): 
+  with open(PATH_TO_IMAGE, "wb+") as f: 
+    f.write(image)
 
 def get_and_write_to_file(song): 
   # print(song)
-  artists = ", ".join([artist['name'] for artist in song['item']['artists']])
-  print(f"new song: {song['item']['name']} by {artists}!")
+  artists = [artist['name'] for artist in song['item']['artists']]
+  title = song['item']['name']
+  print(f"new song: {title} by {', '.join(artists)}!")
   loc_url = song['item']['album']['images'][0]['url']
   url = loc_url
 
   image = requests.get(url)
 
-  file = open(PATH_TO_IMAGE, "wb")
-  file.write(image.content)
-  file.close()
+  write_info(title) 
+  write_img(image.content)
+
+def refresh_token(spot, token):
+  r_token = token['refresh_token']
+  new_token = spot.refresh_access_token(r_token)
+  return spotipy.Spotify(auth=new_token['access_token'])
 
 def main(): 
   scope = 'user-read-currently-playing'
@@ -32,18 +46,24 @@ def main():
   token = util.prompt_for_user_token(username=user, scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri="http://127.0.0.1:8888/callback")
 
   old = None
-  
+      
   if token: 
     spotify = spotipy.Spotify(auth=token)
     print("running properly! listening to your music")
     while True: 
-      current = spotify.currently_playing() 
-
-      if old == None or (current != None and current["item"]["album"]["name"] != old["item"]["album"]["name"]):
-        old = current
-        get_and_write_to_file(current)
       
-      sleep(2)
+      try:  
+        current = spotify.currently_playing() 
+
+        if old == None or (current != None and current["item"]["album"]["name"] != old["item"]["album"]["name"]):
+          old = current
+          get_and_write_to_file(current)
+      
+        sleep(2)
+
+      except spotipy.client.SpotifyException:
+        refresh_token(spotify, token)
+
     
   else: 
     print("invalid token")
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use sdl2::pixels::Color;
-use sdl2::event::{Event, WindowEvent}; 
+use sdl2::event::Event; 
 use sdl2::keyboard::Keycode;
 use sdl2::image::{LoadTexture, InitFlag};
-use sdl2::render::{TextureCreator, Texture};
-use sdl2::video::WindowContext;
+use sdl2::render::{Canvas, TextureCreator};
+use sdl2::video::{Window, WindowContext};
 use std::time::Duration;
 use std::path::Path;
 
 use std::env; 
 
-use notify::{Watcher, RecursiveMode, watcher};
+use notify::{Watcher, RecursiveMode, DebouncedEvent, watcher};
 use std::sync::mpsc::channel; 
 
 static IMG_PATH: &str = "assets/album_img.png";
+static INFO_PATH: &str = "assets/info.txt"; 
+static ASSET_PATH: &str = "assets/"; 
 const DEFAULT_SIZE: u32 = 400; 
 
 fn main() {
@@ -28,8 +30,8 @@ fn main() {
     let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG).unwrap();
     
     let size: u32 = if args.len() == 2 { args[1].parse().unwrap() } else { DEFAULT_SIZE }; 
-   
-    let window = video.window("hehe", size, size)
+
+    let window = video.window("desktify_rs", size, size)
         .position_centered()
         .build()
         .unwrap();
@@ -38,19 +40,19 @@ fn main() {
 
     canvas.set_draw_color(Color::WHITE);
     let texture_creator = canvas.texture_creator();
-
-    let mut texture = update_texture(&texture_creator);
     
-    canvas.copy(&texture, None, None).unwrap();
+    update_texture(&mut canvas, &texture_creator);
+
     canvas.present();
 
     let (tx, rx) = channel();
-    let mut watcher = watcher(tx, Duration::from_millis(20)).unwrap();
-    watcher.watch(IMG_PATH, RecursiveMode::Recursive).unwrap();
+    let mut img_watcher = watcher(tx, Duration::from_millis(20)).unwrap();
+    img_watcher.watch(ASSET_PATH, RecursiveMode::Recursive).unwrap();
     
     // ugly
     let mut image_changed: bool = false;
-    let mut should_change: bool = false;
+    let mut texture_should_change: bool = false;
+    let mut info_changed: bool = false; 
     
     let mut event_pump = sdl_context.event_pump().unwrap();
     
@@ -62,17 +64,24 @@ fn main() {
                 Event::KeyDown {keycode: Some(Keycode::Q), .. } => {
                     break 'main;
                 }
-                Event::Window {win_event: WindowEvent::Resized(x, y), ..} => {
-                    println!("{}, {}", x, y);
-                }
                 _ => {} 
             }
         }
         match rx.recv_timeout(Duration::from_millis(20)) {
-            Ok(_event) => {
+            Ok(event) => {
                 // println!("{:?}", event);
-                // ^^ debug 
-                image_changed = true;
+                // ^^ debug
+                match event {
+                    DebouncedEvent::Write(p) | DebouncedEvent::Create(p) => {
+                        if p.ends_with("info.txt") {
+                            info_changed = true; 
+                        } else if p.ends_with("album_img.png") {
+                            image_changed = true; 
+                        }
+                    },    
+                    DebouncedEvent::NoticeWrite(..) => println!("ignore"),
+                    _ => println!("{:?}", event),
+                }
             },
             Err(e) => 
                 if format!("{:?}", e) != "Timeout".to_string() { 
@@ -83,22 +92,28 @@ fn main() {
         // very crude FSM
         // could and should be replaced
         // please
-        if should_change {
-            texture = update_texture(&texture_creator);
-            canvas.copy(&texture, None, None).unwrap();
-            should_change = false;
+        if texture_should_change {
+            update_texture(&mut canvas, &texture_creator);
+            texture_should_change = false;
         }
-
         if image_changed {
-            should_change = true;
+            texture_should_change = true;
             image_changed = false;
         }
+        if info_changed {
+            let title = std::fs::read_to_string(INFO_PATH).unwrap(); 
+            canvas.window_mut().set_title(&title).unwrap();
+            info_changed = false; 
+        }
         
-        ::std::thread::sleep(Duration::from_millis(1000 / 30));
+        std::thread::sleep(Duration::from_millis(1000 / 30));
         canvas.present();
     }
 }
 
-fn update_texture(tc: &TextureCreator<WindowContext>) -> Texture<'_> {
-    tc.load_texture(Path::new(IMG_PATH)).unwrap()
+fn update_texture(canvas: &mut Canvas<Window>, tc: &TextureCreator<WindowContext>) -> () {
+    match tc.load_texture(Path::new(IMG_PATH)) {
+        Ok(t) => canvas.copy(&t, None, None),
+        Err(..) => canvas.fill_rect(canvas.clip_rect()) // assuming always the same error
+    }; 
 }
